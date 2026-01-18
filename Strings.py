34a743fec058694e192e952ba5ba563b20cdbbe9
ahmad <ahmadaljmal254@gmail.com>
2026-01-19 00:47:58 +0300

    Add My Strings Lesson

---
+++ Strings.py
@@ -0,0 +1,23 @@
+# -----------------
+# -- Strings --
+# -----------------
+myStringOne = 'This is single quote'
+myStringTwo = "This is double quote"
+print(myStringOne)
+print(myStringTwo)
+myStringThree = 'This is single quote "Test"'
+myStringFour = "This is double quote 'Test'"
+print(myStringThree)
+print(myStringFour)
+myStringFive = """" First 
+Second
+Third"""
+myStringSix = ''' First 
+Second
+Third'''
+print(myStringFive)
+print(myStringSix)
+myStringSeven = ''' First "Test"
+Second 'Test' 
+Third'''
+print(myStringSeven)
