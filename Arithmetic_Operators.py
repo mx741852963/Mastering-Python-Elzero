648eb55c5e4239c82cffeb81ee021c79ef6c21af
ahmad <ahmadaljmal254@gmail.com>
2026-02-13 13:53:07 +0300

    Add My Arithmetic operators  Lesson

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,36 @@
+# ----------------------------------------
+# -- Arithmetic operators --
+# [+] addition
+# [-] subtraction
+# [*] multiplication
+# [/] division
+# [%] modulus
+# [**] exponent
+# [//] floor division
+
+print(10 + 10)
+print(-10 + 10)
+
+print(60 - 30)
+print(-30 - 20)
+print(-30 - -20)
+
+print(10 * 3)
+print(5 + 10 * 100)
+print((5 + 10) * 100)
+
+print(100 / 20)
+print(int(100 / 20))
+
+print(8 % 2)
+print(9 % 2)
+print(20 % 5)
+
+print(2 ** 5)
+print(2 ** 1.2)
+print(2 ** -1.2)
+
+print(100 // 20)
+print(110 // 20)
+print(119 // 20)
+print(120 // 20)
