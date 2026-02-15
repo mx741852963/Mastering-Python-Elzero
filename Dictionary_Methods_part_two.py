833cf96715aa45edcc75a5f318c037fab6fed0da
ahmad <ahmadaljmal254@gmail.com>
2026-02-15 22:27:58 +0300

    Add My Dictionary part two  Lesson

---
+++ Dictionary_Methods_part_two.py
@@ -0,0 +1,40 @@
+# ---------------------
+# -- Dictionary Methods
+# ---------------------
+
+# setdefault()
+
+user = {
+    "name": "ahmad",
+}
+print(user)
+print(user.setdefault("age", 36))
+print(user)
+print("=" * 40)
+
+# popitem()
+member = {
+    "name": "ahmad",
+    "skill": "css"
+}
+print(member)
+member.update({"age": 23})
+print(member.popitem())
+print("=" * 40)
+
+# items()
+
+view = {
+    "name": "ahmad",
+    "skill": "css"
+}
+allItems = view.items()
+print(view)
+view["age"] = 23
+print(allItems)
+print("=" * 40)
+
+# fromkeys()
+a = ("myKeyOne", "myKeyTwo", "myKeyThree")
+b = "x"
+print(dict.fromkeys(a, b))
