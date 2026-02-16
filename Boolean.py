d33f9ffb6d3520ece0c4a9a1843cbfdce9bd7a55
ahmad <ahmadaljmal254@gmail.com>
2026-02-17 00:17:21 +0300

    Add My Boolean lesson

---
+++ Boolean.py
@@ -0,0 +1,27 @@
+# --------------
+# --Boolean--
+# --------------
+# [1] In programing you need  to know if your code output is True or False
+# [2] Boolean values are the two constant objects False + True
+name = ' '
+print(name.isspace())
+print("=" * 40)
+print(100 > 200)
+print(100 > 100)
+print(100 > 90)
+print("=" * 40)
+# True Values
+print(bool("Ahmad"))
+print(bool(100))
+print(bool(100.5))
+print(bool(True))
+print(bool([1, 2, 3, 4, 5]))
+print("=" * 40)
+# False Values
+print(bool(0))
+print(bool(""))
+print(bool(''))
+print(bool(False))
+print(bool(()))
+print(bool({}))
+print(bool(None))
