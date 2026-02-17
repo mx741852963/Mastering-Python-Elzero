ecb8c1ae99da19eb90799c0d279de911f21ddfed
ahmad <ahmadaljmal254@gmail.com>
2026-02-17 14:15:32 +0300

    Add My Comparison Operators lesson

---
+++ Comparison.py
@@ -0,0 +1,35 @@
+# --------------
+# --Comparison Operators--
+# --------------
+# [==] Equal
+# [!=] Not Equal
+# [ > ] Greater than
+# [ < ] Less than
+# [ >= ] Greater than or Equal
+# [ <= ] Less than or Equal
+# -----------------------
+
+print(100 == 100)
+print(100 == 100.00)
+print(100 == 110)
+print("=" * 50)
+print(100 != 100)
+print(100 != 100.00)
+print(100 != 110)
+print("=" * 50)
+print(100 > 100)
+print(100 > 100.00)
+print(100 > 110)
+print("=" * 50)
+print(100 < 100)
+print(100 < 100.00)
+print(100 < 110)
+print("=" * 50)
+print(100 >= 100)
+print(100 >= 100.00)
+print(100 >= 110)
+print("=" * 50)
+print(100 <= 100)
+print(100 <= 100.00)
+print(100 <= 110)
+print("=" * 50)
