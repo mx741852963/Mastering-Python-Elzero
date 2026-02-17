b78d6e46a88eca7b10ecc2d809fc1979d2733486
ahmad <ahmadaljmal254@gmail.com>
2026-02-17 14:15:49 +0300

    Add My Assignment Operators lesson

---
+++ Assignment_Operators.py
@@ -0,0 +1,22 @@
+# --------------
+# --Assignment Operators --
+# --------------
+# =
+# +=
+# -=
+# *=
+# /=
+# **=
+# %=
+# //=
+# --------------------------
+x = 10  # var one
+y = 20  # vat two
+# var one = self [Operator] var two
+# var one [Operator] = var two
+# x = x + y
+# x += y
+# x -= y
+# x //= y
+
+print(x)
