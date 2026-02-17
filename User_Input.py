76561571f1479bb15ee238898b3e29023efe4c6b
ahmad <ahmadaljmal254@gmail.com>
2026-02-17 14:52:43 +0300

    Add My User Input lesson

---
+++ User_Input.py
@@ -0,0 +1,9 @@
+# ----------------
+# -- User Input --
+# ----------------
+# input("Press Enter to continue...")
+fName = input("Enter Your First name: ")
+mName = input("Enter Your Middle name: ")
+lName = input("Enter Your Last name: ")
+print(f"Hello {fName.strip().capitalize()} {mName.strip().capitalize():.1s} {lName.strip().capitalize()} ")
+# print(f"Hello {fName.strip().capitalize()} {mName.strip().capitalize()[0]} {lName.strip().capitalize()} ")
