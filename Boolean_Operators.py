936f02beb23218c776da3db199458f9fc8e0bcbd
ahmad <ahmadaljmal254@gmail.com>
2026-02-17 14:16:08 +0300

    Add My Boolean Operator lesson

---
+++ Boolean_Operators.py
@@ -0,0 +1,16 @@
+# --------------
+# --Boolean Operator --
+# --------------
+# and
+# or
+# not
+# ----------------
+
+age = 36
+country = "syria"
+rank = 10
+print(age > 16 and country == "syria" and rank > 7)
+print(age > 16 or country == "syria")
+print(age > 40 or country == "KSA")
+print(age > 40 or country == "KSA" or rank > 7)
+print(not age > 16)
