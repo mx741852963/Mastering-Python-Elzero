cf612955d337ade0439290389a9c4d5b4229f053
ahmad <ahmadaljmal254@gmail.com>
2026-02-26 13:55:31 +0300

    Add My Loop While Else Lesson

---
+++ Loop_While&Else.py
@@ -0,0 +1,14 @@
+# ------------------------------------
+# -- Loop While Else --
+# ------------------------------------
+# While condition_is_true
+# code will Run
+a = 0
+while a < 10:
+    print(a)
+    a += 1
+else:
+    print("Loop is done ")  # True Become False
+
+# while False:
+#     print("Will not print")
