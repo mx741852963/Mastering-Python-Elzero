a00bd831ac0c5115a3a33ffc53b10d7efcb11655
ahmad <ahmadaljmal254@gmail.com>
2026-02-27 16:44:53 +0300

    Add My Function And Return Lesson

---
+++ Function_And_Return.py
@@ -0,0 +1,18 @@
+# -------------------------
+# -- Function And Return --
+# -------------------------
+# [1] A Function is a reusable block of code do a task
+# [2] A Function run when you call it
+# [3] A Function accept element to deal with called [parameters]
+# [4] A Function can do the task without return data
+# [5] A Function can return data after job is done
+# [6] A Function create to prevent dry
+# [7] A Function accept elements when you call it called [arguments]
+# [8] There is A Built_in Functions and user defined functions
+# [9] A Function is for all team and all apps
+# -------------------------
+def function_name():
+    print("Hello Python From Inside Function")
+
+
+function_name()
