5a4ad3df3b6e3763222f3f85b150d1dd7d11f055
ahmad <ahmadaljmal254@gmail.com>
2026-02-28 16:36:16 +0300

    Add Function Default Parameters Lesson

---
+++ Function_Default_Parameters.py
@@ -0,0 +1,13 @@
+# ---------------------------------
+# -- Function Default Parameters --
+# ---------------------------------
+
+def say_hello(name="Unknown", age="Unknown", country="Unknown"):
+    print(f"Hello {name} you are {age} years old and You are From {country}")
+
+
+say_hello("Ahmad", 23, "Syria")
+say_hello("Ali", 45, "Syria")
+say_hello("Ali", 21)
+say_hello("karem", )
+say_hello()
