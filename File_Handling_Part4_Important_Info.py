fcdb8a7b80d1f947cdc3412af4e99b444927ed9f
ahmad <ahmadaljmal254@gmail.com>
2026-03-05 15:02:41 +0300

    Add File Handling Part4 Important Info Lesson

---
+++ File_Handling_Part4_Important_Info.py
@@ -0,0 +1,13 @@
+# ----------------------------------------
+# -- File Handling Part4 Important Info --
+# ----------------------------------------
+# myFile = open(r"C:\Users\ahmad\Desktop\Python-project\ahmad.txt", "a")
+# myFile.truncate(5)
+# myFile = open(r"C:\Users\ahmad\Desktop\Python-project\ahmad.txt", "a")
+# print(myFile.tell())
+# myFile = open(r"C:\Users\ahmad\Desktop\Python-project\ahmad.txt", "r")
+# myFile.seek(4)
+# print(myFile.read())
+# import os
+#
+# os.remove(r"C:\Users\ahmad\Desktop\Python-project\ahmad.txt")
