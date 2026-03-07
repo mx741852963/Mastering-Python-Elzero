a7192f5da9f10d07a73a0ffc648f72fe25cfa53f
ahmad <ahmadaljmal254@gmail.com>
2026-03-07 12:58:55 +0300

    Add Built_In Function part2 Lesson

---
+++ Built_In_Function_part2.py
@@ -0,0 +1,26 @@
+# -----------------------------
+# -- Built In Function part2 --
+# -----------------------------
+# sum()
+# round()
+# range()
+# print()
+# ------------------------------
+# sum(iterable,start)
+a = [1, 8, 19, 20, 12, 10]
+print(sum(a))
+print(sum(a, 40))
+# round(number,numofdigits)
+print(round(150.499))
+print(round(150.555, 2))
+# range(start,end,step)
+print(range(10))
+print(list(range(10)))
+print(tuple(range(10, 20)))
+print(tuple(range(0, 20, 2)))
+# print()
+# print("Hello World How Are You")
+# print("Hello", "World", "How", "Are", "You", sep="@")
+print("First Line", end="___")
+print("Second Line")
+print("Third Line")
