ed06589a8f6fa1bf171faf7374ce6b935c40051a
ahmad <ahmadaljmal254@gmail.com>
2026-03-07 12:39:59 +0300

    Add Built_In Function part1 Lesson

---
+++ Built_In_Function_part1.py
@@ -0,0 +1,29 @@
+# -----------------------------
+# -- Built In Function part1 --
+# -----------------------------
+# all()
+# any()
+# bin()
+# id()
+# ------------------------------
+
+# x = [1, 2, 3, 4, 5, []]
+# if all(x):
+#     print("All Elements is true")
+# else:
+#     print("Theres At Least One Element is false")
+# x = [1, 2, 3, 4, 5, []]
+# if any(x):
+#     print("Theres At Least One Element is True")
+# else:
+#     print("Theres No Any True Elements")
+# print(bin(12))
+# print(hex(12))
+# print(oct(12))
+# a = 1
+# b = 2
+# print(id(a))
+# print(id(b))
+# x = [-1, 0.25]
+# if all(x):
+#     print("x is true")
