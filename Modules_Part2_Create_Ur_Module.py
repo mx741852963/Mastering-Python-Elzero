82908ecf96bfb92a706f53172e3ed7863909c12a
ahmad <ahmadaljmal254@gmail.com>
2026-03-10 15:27:54 +0300

    Add Modules Part2 Create your Module Lesson

---
+++ Modules_Part2_Create_Ur_Module.py
@@ -0,0 +1,27 @@
+# ------------------------------------
+# -- Modules Part2 Create Ur Module --
+# ------------------------------------
+# import sys
+# sys.path.append(r"D:\Courses")
+# print(sys.path)
+# import ahmad
+#
+# print(dir(ahmad))
+# ahmad.say_hay("ahmad")
+# ahmad.say_hay("ali")
+# ahmad.say_hay(1)
+# ahmad.say_hay(2)
+# ahmad.say_hay(3)
+# Alias
+# import ahmad as ah
+#
+# # print(dir(ahmad))
+# ah.say_hay("ahmad")
+# ah.say_hay("ali")
+# ah.say_hay(1)
+# ah.say_hay(2)
+# ah.say_hay(3)
+
+from ahmad import say_hello as sh
+
+sh("ahmad")
---
+++ ahmad.py
@@ -0,0 +1,6 @@
+def say_hello(name):
+    print(f"Hello {name} ")
+
+
+def say_hay(name):
+    print(f"How Are You {name} ")
