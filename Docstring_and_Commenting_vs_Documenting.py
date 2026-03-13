2aaa4c222f4b045f013a9e3e5bf58d4aab692d7e
ahmad <ahmadaljmal254@gmail.com>
2026-03-14 00:45:09 +0300

    Add Doc string and Commenting vs Documenting Lesson

---
+++ Docstring_and_Commenting_vs_Documenting.py
@@ -0,0 +1,21 @@
+# ----------------------------------------------
+# -- Doc string and Commenting vs Documenting --
+# ----------------------------------------------
+# [1] Documentation String For Class,Module,Or Function
+# [2] Can be accessed from the help and doc attribute
+# [3] made for understanding the functionality of the complex code
+# [4] there is one line and multiple line doc string
+# ----------------------------------------------------------------
+def elzero_function(name):
+    """ Elzero Function
+        It say hello from elzero
+        parameter
+            name : str
+        return
+            hello message"""
+    print(f"Hello {name} from Elzero")
+
+# elzero_function("Ahmad")
+# print(dir(elzero_function))
+# print(elzero_function.__doc__)
+# help(elzero_function)
