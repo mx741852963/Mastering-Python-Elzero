15fabcce0c82e1cddcbeb505bad8b9a3cbaa939a
ahmad <ahmadaljmal254@gmail.com>
2026-03-14 01:07:24 +0300

    Add Installing And Use Pylint For Better Code Lesson

---
+++ Pylint_For_Better_code.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------
+# -- Installing And Use Pylint For Better Code --
+# -----------------------------------------------
+def sayHello(name):
+    msg = "Hello "
+    return msg + name
+
+
+print(sayHello("Ahmad"))
---
+++ elzero.py
@@ -0,0 +1,10 @@
+""" Elzero Function"""
+
+
+def say_hello(name):
+    '''This function says hello to name'''
+    msg = "Hello "
+    return msg + name
+
+
+print(say_hello("Ahmad"))
