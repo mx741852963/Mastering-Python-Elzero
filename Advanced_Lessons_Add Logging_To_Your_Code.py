370a1f5bf589a9b87cdf65077e0ac057dd9ac7f9
ahmad <ahmadaljmal254@gmail.com>
2026-03-24 20:46:05 +0300

    Add  Logging To Your Code Lesson

---
+++ Advanced_Lessons_Add Logging_To_Your_Code.py
@@ -0,0 +1,33 @@
+# --------------------------------------------------
+# -- Advanced_Lessons => Add Logging To Your Code --
+# --------------------------------------------------
+# - Print Out To Console Or File
+# - Print Logs Of What Happens
+# ------------------------------
+# - DEBUG
+# - INFO
+# - WARNING
+# - ERROR
+# - CRITICAL
+# ----------
+# name => Logging Module Give It To The Default Logger.
+# -----------------------------------------------------
+# Basic Config
+# - level => Level of Severity
+# - filename => File Name and Extension
+# - mode => Mode Of The File a => Append
+# - format => Format For The Log Message
+# ------------------------
+# getLogger => Return a Logger With the Specified Name
+import logging
+
+# print(dir(logging))
+logging.basicConfig(filename='my_app.log',
+                    filemode="a",
+                    format="(%(asctime)s) - %(name)s - %(levelname)s - %(message)s",
+                    datefmt="%d-%B-%Y %H:%M:%S",
+                    )
+my_logger = logging.getLogger('Ahmad')
+my_logger.critical("THIS IS CRITICAL")
+my_logger.warning("THIS IS Warning")
+my_logger.error("THIS IS ERROR")
