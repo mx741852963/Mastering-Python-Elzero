7bc67e935ae01785d29d972d19ed22f88b0d104d
ahmad <ahmadaljmal254@gmail.com>
2026-03-24 20:27:39 +0300

    Add Timing Your Code With Timeit Lesson

---
+++ Advanced_Lessons_Timing_Your_Code_With_Timeit.py
@@ -0,0 +1,27 @@
+# ------------------------------------------------------
+# -- Advanced_Lessons => Timing Your Code With Timeit --
+# ------------------------------------------------------
+# - timeit: - Get Execution Time Of Code By Running 1M Time And Give You Minimal Time
+# -         - It Used For Performance By Testing All Functionality
+# - timeit(stmt, setup, timer, number)
+# - timeit(pass, pass, default, 1.000.000) Default Values
+# -------------------------------------------------------
+# - stmt: Code You Want To Measure The Execution Time
+# - setup: Setup Done Before The Code Execution (Import Module Or Anything)
+# - timer: The Timer Value
+# - number: How Many Execution That Will Run
+# -------------------------------------------------------
+
+import timeit
+import time
+
+# import random
+
+# print(dir(timeit))
+# print(timeit.timeit("'Elzero'*1000"))
+# name = "Elzero"
+# print(name * 1000)
+# print(timeit.timeit("name = 'Elzero';name * 1000"))
+print(timeit.timeit(stmt="random.randint(1,100)", setup="import random", timer=time.perf_counter))  # Wall Clock
+print(timeit.timeit(stmt="random.randint(1,100)", setup="import random", timer=time.process_time))  # CPU Time
+# print(timeit.repeat(stmt="random.randint(1,100)", setup="import random", repeat=4))
