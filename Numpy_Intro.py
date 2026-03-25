2afec213e2315557f90c9673aefb884f571ddbba
ahmad <ahmadaljmal254@gmail.com>
2026-03-25 10:01:31 +0300

    Numpy => Intro

---
+++ Numpy_Intro.py
@@ -0,0 +1,30 @@
+# --------------------
+# -- Numpy => Intro --
+# --------------------
+# - NumPy Is A Python Third-Party Module To Deal With Arrays & Matrices
+# - NumPy Stand For Numerical Python
+# - NumPy Is Open Source
+# - NumPy Support Dealing With Large Multidimensional Arrays & Matrices
+# - NumPy Has Many Mathematical Functions To Deal With This Elements
+# ------------------------------------------------------------
+# * Why We Use NumPy Array
+# - Consume Less Memory
+# - Very Fast Compared To Python List
+# - Easy To Use
+# - Support Element Wise Operation
+# - Elements Are Stored Contiguous
+# -------------------------------------------
+# * Python Lists
+# ---- Homogeneous => Can Contain The Same Type of Objects
+# ---- Heterogeneous => Can Contain Different Types of Objects.
+# --------------------------------------------------------------
+# - The Items in The Array Have to Be Of The Same Type
+# - You Can Be Sure What's The Storage Size Needed for The Array
+# - NumPy Arrays Are Indexed From 0
+# --------------------------------------------------------------
+# - NumPy On Github => https://github.com/numpy/numpy
+# ---------------------------------------------------
+
+import numpy as np
+
+print(np.__version__)
